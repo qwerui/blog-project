8e9c7922bf7a2f90d05b1d8c69d0f986d7cf2c15
Jegi Hong <39932013+qwerui@users.noreply.github.com>
2025-01-28 17:37:10 +0900

    블로그 사이드바 레이아웃 생성

--- blog-frontend/src/contents/blog-comp/blog-nav.js
+++ blog-frontend/src/contents/blog-comp/blog-nav.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const blogNav = function BlogNav(){
+const blogNav = function BlogNav() {
 
-    return(
+    const [category, setCategory] = useState([]);
+    const [profile, setProfile] = useState("");
+
+    return (
         <>
-            <aside className='w-25 bg-primary d-inline-block h-100'>Nav</aside>
+            <aside className='w-25 d-inline-block h-100 p-3'>
+                <div id="blog-profile-image" className='d-flex justify-content-center'>
+                    <img className='border border-3' style={{height: "200px", width: "200px"}} src={profile ? profile : "/logo512.png"} alt='profile'/>
+                </div>
+                <div id="blog-owner-description" className='border mt-1'>
+                    <h5 className="text-center mt-2">블로그 주인장</h5>
+                    <hr/>
+                    <p className='ps-2 pe-2'>여기에 간단한 자기소개가 나옵니다.</p>
+                </div>
+                <ul className="list-group mt-2">
+                    <li className="list-group-item fw-bold">카테고리</li>
+                    {category.map(item=><li className="list-group-item">{item}</li>)}
+                </ul>
+            </aside>
         </>
     )
 }
--- blog-frontend/src/contents/blog.js
+++ blog-frontend/src/contents/blog.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {Outlet} from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import BlogNav from './blog-comp/blog-nav'
 
-const blog = function Blog(){
+const blog = function Blog() {
 
-    return(
+    return (
         <>
-            <BlogNav/>
-            <section className='w-75 bg-secondary d-inline-block h-100'>
-                <Outlet/>
-            </section>
+            <div id="blog-wrapper" className='h-100 d-flex'>
+                <BlogNav />
+                <section className='w-75 bg-secondary d-inline-block h-100'>
+                    <Outlet />
+                </section>
+            </div>
         </>
     )
 }
