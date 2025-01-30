209528fcda6ac1e617ef7b5d9f7020caa8e42f47
Jegi Hong <39932013+qwerui@users.noreply.github.com>
2025-01-30 17:20:08 +0900

    글쓰기 Quill 적용

--- blog-frontend/src/contents/write.js
+++ blog-frontend/src/contents/write.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import Editor from 'layout/editor';
+import Quill from 'quill';
 
-const blogWrite = function blogWrite(){
+const Delta = Quill.import('delta');
 
-    return(
+const blogWrite = function BlogWrite() {
+    // Use a ref to access the quill instance directly
+    const quillRef = useRef();
+
+    const uploadArticle = ()=>{
+        console.log(quillRef.current.getSemanticHTML());
+    }
+
+    return (
         <>
-            <div>Write</div>
+            <h1>글쓰기</h1>
+            <hr />
+            <Editor
+                ref={quillRef}
+            />
+            <hr/>
+            <button className='btn btn-primary w-100 mb-5' onClick={()=>uploadArticle()}>글쓰기</button>
         </>
     )
 }
