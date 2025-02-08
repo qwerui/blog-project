8dc19c58f0170c33949c177a74a30750b689f6ce
Jegi Hong <39932013+qwerui@users.noreply.github.com>
2025-02-08 21:19:28 +0900

    프런트엔드-백엔드 연동

--- blog-frontend/src/contents/write.js
+++ blog-frontend/src/contents/write.js
@@ -1,10 +1,47 @@
 import React from 'react';
 
-const blogWrite = function blogWrite(){
+const blogWrite = function BlogWrite() {
+    // Use a ref to access the quill instance directly
+    const quillRef = useRef();
 
-    return(
+    const uploadArticle = () => {
+        console.log(quillRef.current.getSemanticHTML());
+    }
+
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState({});
+
+    return (
         <>
-            <div>Write</div>
+            <h1>글쓰기</h1>
+            <hr />
+            <div className='d-flex justify-content-between'>
+                <div className='d-flex'>
+                    <div className="dropdown">
+                        <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {selectedCategory.name ? selectedCategory.name : "카테고리"}
+                        </button>
+                        <ul className="dropdown-menu">
+                            {category.map(item => {
+                                return (
+                                    <li className="dropdown-item" onClick={() => setSelectedCategory(item)}>{item.name}</li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
+
+                <button className='btn btn-primary' onClick={() => uploadArticle()}>글쓰기</button>
+            </div>
+            <div>
+                제목<input className='form-control' onChange={(e)=>setTitle(e.target.value)}/>
+            </div>
+
+            <hr />
+            <Editor
+                ref={quillRef}
+            />
         </>
     )
 }
