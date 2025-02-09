c4c300d138377818cb971fdafb99494176dc3a62
Jegi Hong <39932013+qwerui@users.noreply.github.com>
2025-02-09 22:23:09 +0900

    프런트 https 적용

---
+++ blog-frontend/src/util/token-refresh.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import store from "data/store";
+import { refresh } from "data/login-store";
+import { jwtDecode } from "jwt-decode";
+import config from 'config.json';
+
+const refreshToken = async () => {
+
+    const state = store.getState();
+    const access = state.login.token;
+    const isLogin = state.login.isLogin;
+    const loginId = state.login.id;
+
+    if(!isLogin){
+        return;
+    }
+    
+    try {
+        const { exp } = jwtDecode(access);
+
+        if (exp < Math.floor(Date.now() / 1000)){
+            const newToken = await axios.post(config["auth-backend"]+"/auth/refresh",{id:loginId},{withCredentials: true});
+
+            store.dispatch(refresh(newToken));
+        }
+    } catch (error) {
+        console.error("Token refresh error : ", error);
+        return;
+    }
+}
+
+export default refreshToken;
