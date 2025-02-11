fe8848ccbef06edd5e53c388855f078cf565ddbd
Jegi Hong <39932013+qwerui@users.noreply.github.com>
2025-02-11 18:51:30 +0900

    블로그 화면 작성

--- blog-frontend/src/contents/blog-comp/blog-list-option.js
+++ blog-frontend/src/contents/blog-comp/blog-list-option.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import formatter from 'util/custom-formatter';
 
-const blogListOption = function blogListOption({item}){
+const blogListOption = function blogListOption({item, id}){
 
     return(
         <>
-            <li class="list-group-item d-flex justify-content-between">
-                <Link to={item.link}><span>{item.title}</span></Link>
-                <span>{item.createdDate}</span>
+            <li class="list-group-item d-flex justify-content-between" key={item.article_id}>
+                <Link to={"/blog/"+id+"/content/"+item.article_id}><span>{item.title}</span></Link>
+                <span>{formatter.ConvertArticleTime(item.create_time)}</span>
             </li>
         </>
     )
--- blog-frontend/src/contents/blog-comp/blog-list.js
+++ blog-frontend/src/contents/blog-comp/blog-list.js
@@ -1,40 +1,99 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useLocation, Link } from 'react-router-dom';
 
 import BlogListOption from './blog-list-option';
+import axios from 'axios';
+import config from 'config.json';
 
 const blogList = function BlogList() {
 
+    const location = useLocation();
+
     const [listOption, setListOption] = useState([]);
+    const [categoryName, setCategoryName] = useState("");
+    const [pagination, setPagination] = useState([]);
+    const [isEmpty, setIsEmpty] = useState(false);
+    const id = useRef("");
+    const category = useRef();
+    const page = useRef(0);
+    const totalPage = useRef(0);
+
+    useEffect(() => {
+        id.current = location.pathname.split("/")[2];
+        const search = new URLSearchParams(location.search);
+        category.current = search.get("category");
+        const name = search.get("name") ?? "전체글보기";
+        page.current = search.get("page") ?? 0;
+
+        setCategoryName(name);
+
+        const fetch = async () => {
+            try {
+                const response = await axios.get(config["blog-backend"] + "/api/blog/list", {
+                    params: {
+                        id: id.current,
+                        category: category.current,
+                        page: page.current
+                    }
+                });
+
+                setListOption(response.data.articles);
+                totalPage.current = response.data.totalPage;
+                setPagination(getPaginationNumbers(page.current, totalPage.current));
+            } catch (err) {
+                if (err.state === 404) {
+                    setIsEmpty(true);
+                }
+            }
+        }
+        fetch();
+    }, [location]);
+
+    function getPaginationNumbers(currentPage, totalPages, maxVisiblePages = 5) {
+        const half = Math.floor(maxVisiblePages / 2);
+        let start = Math.max(1, currentPage - half);
+        let end = Math.min(totalPages, start + maxVisiblePages - 1);
+
+        start = Math.max(1, end - maxVisiblePages + 1);
+
+        return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+    }
 
     return (
         <>
             <div className='p-3'>
                 <div className='border'>
-                    <h3 className='ps-3 pt-2'>카테고리 이름</h3>
+                    <h3 className='ps-3 pt-2'>{categoryName}</h3>
                     <hr />
-                    <ul class="list-group">
-                        {listOption.map(item=><BlogListOption item={item}/>)}
-                    </ul>
-                    <nav className='d-flex justify-content-center mt-2'>
-                        <ul class="pagination">
-                            <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Previous">
-                                    <span aria-hidden="true">&laquo;</span>
-                                </a>
-                            </li>
-                            <li class="page-item"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="#" aria-label="Next">
-                                    <span aria-hidden="true">&raquo;</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
+                    {
+                        isEmpty ?
+                            <>
+                                <ul className="list-group">
+                                    {listOption.map(item => <BlogListOption item={item} id={id.current} />)}
+                                </ul>
+                                <nav className='d-flex justify-content-center mt-2'>
+                                    <ul className="pagination">
+                                        <li className="page-item">
+                                            <Link className={page.current === 0 ? "page-link disabled" : "page-link"} to={{ pathname: `/blog/${id.current}/list`, search: `?category=${category.current}&page=${page.current - 2}&name=${categoryName}` }} aria-label="Previous">
+                                                <span aria-hidden="true">&laquo;</span>
+                                            </Link>
+                                        </li>
+                                        {
+                                            pagination.map(item =>
+                                                <li className="page-item"><Link className={page.current === item - 1 ? "page-link active" : "page-link"} to={{ pathname: `/blog/${id.current}/list`, search: `?category=${category.current}&page=${item - 1}&name=${categoryName}` }}>{item}</Link></li>
+                                            )
+                                        }
+                                        <li className="page-item">
+                                            <Link className={page.current === totalPage.current - 1 ? "page-link disabled" : "page-link"} to={{ pathname: `/blog/${id.current}/list`, search: `?category=${category.current}&page=${page.current}&name=${categoryName}` }} aria-label="Previous">
+                                                <span aria-hidden="true">&raquo;</span>
+                                            </Link>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </> :
+                            <h2>게시글이 존재하지 않습니다.</h2>
+                    }
                 </div>
-
-
             </div>
         </>
     )
