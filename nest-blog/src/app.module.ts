264934a8147560e92db57508b38cba2296e8f5d8
Jegi Hong <39932013+qwerui@users.noreply.github.com>
2025-05-22 19:44:26 +0900

    기본 서비스 및 컨트롤러 생성

--- nest-blog/src/app.module.ts
+++ nest-blog/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlogController } from './blog/blog.controller';
+import { ConfigService } from './config/config.service';
+import { ConfigController } from './config/config.controller';
+import { WriteService } from './write/write.service';
+import { WriteController } from './write/write.controller';
+import { BlogService } from './blog/blog.service';
+import { BlogController } from './blog/blog.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, BlogController, WriteController, ConfigController],
+  providers: [AppService, BlogService, WriteService, ConfigService],
 })
 export class AppModule {}
---
+++ nest-blog/src/write/write.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('write')
+export class WriteController {}
