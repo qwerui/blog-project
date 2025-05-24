7ac85ba31ac02b1c8bb0c5e5b6dfa39350ede66b
Jegi Hong <39932013+qwerui@users.noreply.github.com>
2025-05-24 13:23:13 +0900

    https 적용

--- nest-blog/src/main.ts
+++ nest-blog/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+  }
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: httpsOptions
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
