8f4dec24a74d0c58d3ba27d68d81b7f8a4a38939
Jegi Hong <39932013+qwerui@users.noreply.github.com>
2025-05-25 09:56:48 +0900

    환경설정
    - DB 연결, 환경변수 연결, Cors

--- nest-blog/src/app.module.ts
+++ nest-blog/src/app.module.ts
@@ -7,10 +7,25 @@ import { ConfigController } from './config/config.controller';
 import { WriteService } from './write/write.service';
 import { WriteController } from './write/write.controller';
 import { BlogService } from './blog/blog.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+const mysql = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'blog',
+  entities: [],
+  synchronize: true,
+});
 
 @Module({
-  imports: [],
+  imports: [mysql, ConfigModule.forRoot()],
   controllers: [AppController, BlogController, WriteController, ConfigController],
   providers: [AppService, BlogService, WriteService, ConfigService],
 })
+
 export class AppModule {}
+
--- nest-blog/src/main.ts
+++ nest-blog/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'fs';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  
+  // https
   const httpsOptions = {
     key: fs.readFileSync('./key.pem'),
     cert: fs.readFileSync('./cert.pem'),
@@ -10,6 +14,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     httpsOptions: httpsOptions
   });
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 입력값 검증
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: "https://localhost:3000",
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 8080);
 }
+
 bootstrap();
